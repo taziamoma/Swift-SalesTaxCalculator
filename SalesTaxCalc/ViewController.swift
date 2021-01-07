f4b71489dfc77176a4a6b5826278c7e8df462b10
Taziamoma Abraham <taziamoma@gmail.com>
2021-01-07 13:30:57 -0600

    Initial Commit

---
+++ SalesTaxCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SalesTaxCalc
+//
+//  Created by Taziamoma Abraham on 1/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
